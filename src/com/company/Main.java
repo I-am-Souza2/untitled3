8c764d4fe389d025eadaecbf3d4fb6a04407d240
Thales <82174699+yFrosttty@users.noreply.github.com>
2022-02-17 17:05:56 -0300

    teste

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here.
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+        String sobname = scanner.nextLine();
+
+        System.out.println(name + sobname);
+
+        scanner.close();
+
+
+
     }
 }
